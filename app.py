b88319db99b9bc161a5c7183d2e59ab1a68da6d2
bharat <bharatkumarmakhija14@gmail.com>
2017-06-17 18:38:07 +0530

    added more logs

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def verify():
 def webhook():
 
     # endpoint for processing incoming messaging events
-    log("message came from facebook....................................................................... ")
+    #log("message came from facebook....................................................................... ")
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
