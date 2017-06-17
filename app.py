575f74dd51c5efbed74fc021b5f9ef728a638a18
bharat <bharatkumarmakhija14@gmail.com>
2017-06-17 18:20:37 +0530

    checking if text is there in msg object

--- app.py
+++ app.py
@@ -38,8 +38,9 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                  #  message_text = messaging_event["message"]["text"]  # the message's text
-
+                    if "text" in messaging_event:
+                        message_text = messaging_event["message"]["text"]
+                        log("..........................................................message sent by user is == " + message_text)
                     send_message(sender_id, "roger that!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
